f651d80a87de5792a1448358f7836833ee23782e
Guilherme da Costa de Albuquerque <guicalbuquerque@gmail.com>
2023-08-27 12:05:15 -0300

    Add project files.

---
+++ Models/ProductModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LojaBlazorMaui.App.Models
+{
+	public class ProductModel
+	{
+		public Guid? Id { get; set; }
+		public string Foto { get; set; }
+		public string Nome { get; set; }
+		public string Preco { get; set; }
+	}
+}
