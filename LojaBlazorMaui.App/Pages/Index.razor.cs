9d0759996165b928045178d808bf4199c5437138
Guilherme da Costa de Albuquerque <guicalbuquerque@gmail.com>
2023-09-03 09:57:49 -0300

    feat: implementa lógica para exibir dados do produto dentro da modal.

--- LojaBlazorMaui.App/Pages/Index.razor.cs
+++ LojaBlazorMaui.App/Pages/Index.razor.cs
@@ -3,18 +3,34 @@ using LojaBlazorMaui.Services;
 
 namespace LojaBlazorMaui.App.Pages
 {
-	public partial class Index
-	{
-		private List<ProdutosGetModel> produtos = new();
+    public partial class Index
+    {
+        /// <summary>
+        /// Produtos exibidos na página inicial da loja
+        /// </summary>
+        private List<ProdutosGetModel> produtos = new();
 
-		/// <summary>
-		/// Método executado quando a página é carregada
-		/// </summary>
-		/// <returns></returns>
-		protected override async Task OnInitializedAsync()
-		{
-			var produtosService = new ProdutosService();
-			produtos = await produtosService.GetAll();
-		}
-	}
+        /// <summary>
+        /// Dados do produto selecionado pelo usuário
+        /// </summary>
+        private ProdutosGetModel produtoSelecionado = new();
+
+        /// <summary>
+        /// Método executado quando a página é carregada
+        /// </summary>		
+        protected override async Task OnInitializedAsync()
+        {
+            var produtosService = new ProdutosService();
+            produtos = await produtosService.GetAll();
+        }
+
+        /// <summary>
+        /// Método para capturar 1 produto selecionado pelo usuário
+        /// </summary>
+        /// <param name="item"></param>		
+        protected async Task SelecionarProduto(ProdutosGetModel item)
+        {
+            produtoSelecionado = item;
+        }
+    }
 }
